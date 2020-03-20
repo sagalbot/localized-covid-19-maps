faba0ae866e96e0f5806800100c4f2c5a4775864
Jeff <sagalbot@gmail.com>
2020-03-19 19:15:10 -0700

    run prettier

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -38,10 +38,7 @@ class Kernel extends HttpKernel
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
 
-        'api' => [
-            'throttle:60,1',
-            \Illuminate\Routing\Middleware\SubstituteBindings::class,
-        ],
+        'api' => ['throttle:60,1', \Illuminate\Routing\Middleware\SubstituteBindings::class],
     ];
 
     /**
