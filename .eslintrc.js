743256b19b1b7a10b0069524f679e7ef749f08e7
Jeff <sagalbot@gmail.com>
2020-03-19 18:56:16 -0700

    install lint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,7 @@
 module.exports = {
-  extends: ['plugin:vue/recommended', 'plugin:prettier/recommended']
+  extends: ['plugin:vue/recommended', 'plugin:prettier/recommended'],
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: 'module'
+  }
 };
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,15 +1,17 @@
-import { InertiaApp } from '@inertiajs/inertia-vue'
-import Vue from 'vue'
+import { InertiaApp } from '@inertiajs/inertia-vue';
+import Vue from 'vue';
 
-Vue.use(InertiaApp)
+Vue.use(InertiaApp);
 
-const app = document.getElementById('app')
+const app = document.getElementById('app');
 
 new Vue({
-  render: h => h(InertiaApp, {
-    props: {
-      initialPage: JSON.parse(app.dataset.page),
-      resolveComponent: name => import(`./Pages/${name}`).then(module => module.default),
-    },
-  }),
-}).$mount(app)
+  render: h =>
+    h(InertiaApp, {
+      props: {
+        initialPage: JSON.parse(app.dataset.page),
+        resolveComponent: name =>
+          import(`./Pages/${name}`).then(module => module.default)
+      }
+    })
+}).$mount(app);
