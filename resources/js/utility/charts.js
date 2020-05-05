73c169baccea636a333ffaf5e0f4a4811f8257e3
Jeff Sagal <sagalbot@gmail.com>
2020-05-04 18:35:54 -0700

    feat: cases per day (#15)

--- resources/js/utility/charts.js
+++ resources/js/utility/charts.js
@@ -1,36 +1,39 @@
 import { chartColors } from './colors';
+import { merge } from 'lodash-es';
 import abbr from 'sugar/number/abbr';
 
-const chartConfig = (options = {}) => ({
-  chart: {
-    redrawOnParentResize: false,
-    fontFamily:
-      'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"'
-  },
-  colors: chartColors(),
-  stroke: {
-    show: true,
-    lineCap: 'butt',
-    width: 2,
-    dashArray: 0
-  },
-  markers: {
-    size: 2,
-    strokeWidth: 0,
-    hover: {
-      sizeOffset: 0
-    }
-  },
-  yaxis: {
-    labels: {
-      formatter: value => {
-        const precision = parseFloat(value) >= 100000 ? 1 : 2;
-        return abbr(value, precision);
+const chartConfig = (options = {}) =>
+  merge(
+    {
+      chart: {
+        redrawOnParentResize: false,
+        fontFamily:
+          'system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"'
+      },
+      colors: chartColors(),
+      stroke: {
+        show: true,
+        lineCap: 'butt',
+        width: 2,
+        dashArray: 0
+      },
+      markers: {
+        size: 2,
+        strokeWidth: 0,
+        hover: {
+          sizeOffset: 0
+        }
+      },
+      yaxis: {
+        labels: {
+          formatter: value => {
+            const precision = parseFloat(value) >= 100000 ? 1 : 2;
+            return abbr(value, precision);
+          }
+        }
       }
-    }
-  },
-
-  ...options
-});
+    },
+    options
+  );
 
 export { chartConfig };
